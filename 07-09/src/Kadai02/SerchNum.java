8bf3a804df3a8af47401a19d60f7bbb666128296
Shintaro Matsumoto <matsushin35@gmail.com>
2021-07-25 03:48:55 +0900

    Implemented AveNum Class

--- 07-09/src/Kadai02/SerchNum.java
+++ 07-09/src/Kadai02/SerchNum.java
@@ -1,12 +1,22 @@
 package Kadai02;
 
+import java.util.Arrays;
+
 public abstract class SerchNum {
     private int[] num = new int[5];
     void setNum(int num[]) {
         this.num = num;
     }
+    abstract int getNum();
 }
 
 class AveNum extends SerchNum {
-
+    int getAve(int[] num) {
+        int sum = Arrays.stream(num).sum();
+        return sum / num.length;
+    }
+    @Override
+    int getNum() {
+        return 0;
+    }
 }
