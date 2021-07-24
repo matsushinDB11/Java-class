af9664d03c3dae98580d18aa06af3382c1f2ac95
matsushinDB11 <matsushin35@gmail.com>
2021-07-24 19:42:03 +0900

    rm library

--- 07-09/src/Kadai01/Main.java
+++ 07-09/src/Kadai01/Main.java
@@ -1,12 +1,10 @@
 package Kadai01;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) throws IOException{
