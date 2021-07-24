c8fbea96fb0aa56daeb2c4d89d19dae95ed96ffa
matsushinDB11 <matsushin35@gmail.com>
2021-07-24 12:52:30 +0900

    add music data

--- 07-09/src/Kadai01/Main.java
+++ 07-09/src/Kadai01/Main.java
@@ -1,6 +1,17 @@
 package Kadai01;
 
 public class Main {
-    MusicData MusicDB[] = new MusicData[10];
-
+    public static void main(String[] args) {
+        MusicData[] MusicDB = new MusicData[10];
+        MusicDB[0].setFields("ENDLESS RAIN", "X", "BLUE BLOOD", 1989);
+        MusicDB[1].setFields("Dejavu", "LUNA SEA", "IMAGE", 1992);
+        MusicDB[2].setFields("DAHLIA", "X JAPAN", "DAHLIA", 1996);
+        MusicDB[3].setFields("M", "浜崎あゆみ", "M", 2000);
+        MusicDB[4].setFields("そばにいるね" , "青山テルマ", "そばにいるね", 2008);
+        MusicDB[5].setFields("めぐろ川", "miwa", "don't cray any more", 2010);
+        MusicDB[6].setFields("制服のマネキン", "乃木坂46", "制服のマネキン", 2012);
+        MusicDB[7].setFields("BABYMETAL DEATH", "BEBYMETAL", "BABYMETAL", 2014);
+        MusicDB[8].setFields("サイレントマジョリティー", "欅坂46", "サイレントマジョリティー", 2016);
+        MusicDB[9].setFields("キュン", "日向坂46", "キュン", 2019);
+    }
 }
