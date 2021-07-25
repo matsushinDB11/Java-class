a5b0c4117d3e2502ccdd8d9ebb1b5cc7dd2809d9
Shintaro Matsumoto <matsushin35@gmail.com>
2021-07-25 10:06:55 +0900

    done Kadai02

--- 07-09/src/Kadai02/SearchNum.java
+++ 07-09/src/Kadai02/SearchNum.java
@@ -1,13 +1,18 @@
 package Kadai02;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public abstract class SearchNum {
-    private int[] num = new int[5];
+    int[] num = new int[5];
     void setNum(int num[]) {
         this.num = num;
     }
     abstract int getNum();
+    abstract int getNum(int index);
+    void output_all() {
+        for (int i = 0; i < 5; i++) System.out.println(getNum(i));
+    };
 }
 
 interface SortNum {
@@ -15,20 +20,57 @@ interface SortNum {
 }
 
 class AveNum extends SearchNum {
-    int getAve(int[] num) {
+    int getNum() {
         int sum = Arrays.stream(num).sum();
         return sum / num.length;
     }
+
+    @Override
+    int getNum(int index) {
+        return 0;
+    }
+
+    void output() {
+        System.out.println("平均値: " + getNum());
+    }
+}
+
+class SortAscNum extends SearchNum implements SortNum{
+    int getNum(int index) {
+        this.num_sort();
+        return num[index];
+    }
+
     @Override
     int getNum() {
         return 0;
     }
+
+    @Override
+    public void num_sort() {
+        Arrays.sort(this.num);
+    }
 }
 
-class SortAscNum extends SearchNum {
+class  SortDescNum extends SearchNum implements SortNum{
+    int getNum(int index) {
+        this.num_sort();
+        return num[index];
+    }
 
     @Override
     int getNum() {
         return 0;
+    }
+    @Override
+    public void num_sort() {
+        Integer[] list = new Integer[5];
+        for (int i = 0; i < 5; i++) {
+            list[i] = this.num[i];
+        }
+        Arrays.sort(list, Collections.reverseOrder());
+        for (int i = 0; i < 5; i++) {
+            this.num[i] = list[i];
+        }
     }
 }
